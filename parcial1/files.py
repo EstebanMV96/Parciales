081670473e9872289b84b793b193e4abff766670
EstebanMV96 <moyaco41@gmail.com>
2016-10-19 14:43:59 -0500

    Update files.py

--- parcial1/files.py
+++ parcial1/files.py
@@ -9,7 +9,6 @@ def darArchivos():
   list["files"] = lsArchivos()
   return json.dumps(list), 200
 
-
 @app.route('/files',methods=['DELETE'])
 def eliminarArchivos():
 	
@@ -18,12 +17,24 @@ def eliminarArchivos():
 			
 	return "Todos los archivos no VIP fueron borrados",200
 
-
 @app.route('/files/recently_created',methods=['GET'])
 def recently():
   list = {}
   list["Archivos creados hace menos de un dia"] = darArchivosRecientes()
   return json.dumps(list),200
+  
+@app.route('/files',methods=['POST'])
+def newArchivo():
+  content = request.get_json(silent=True)
+  nombreA = content['filename']
+  contenido= content['content']
+  if crearArchivo(nombreA,contenido):
+  	return "Archivo creado",201
+  
+@app.route('/files/recently_created',methods=['POST'])
+def recently3():
+ 
+  return "No implementado",404
 
 @app.route('/files/recently_created',methods=['PUT'])
 def recently1():
@@ -40,25 +51,5 @@ def recently5():
  
   return "No implementado",404
 
-
-@app.route('/files',methods=['POST'])
-def newArchivo():
-  content = request.get_json(silent=True)
-  nombreA = content['filename']
-  contenido= content['content']
-  if crearArchivo(nombreA,contenido):
-  	return "Archivo creado",201
-  
-#@app.route('/files/recently_created',methods=['POST'])
-#def recently3():
- 
-  #return "No implementado",404
-
-
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',port=8080,debug='True')
-
-
-
-
-
